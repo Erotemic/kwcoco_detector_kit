d9787b59cf924b6e481ffc6c8ab79dae75723541
joncrall <erotemic@gmail.com>
2026-09-20 17:09:24 -0400

    Archive source command

--- archive_source.sh
+++ archive_source.sh
@@ -7,6 +7,12 @@ stack/delayed_image: 1
 stack/kwcoco: 100
 stack/kwcoco_dataloader: 100
 tpl/DEIMv2: 10
-tpl/Open-GroundingDino: 10
+tpl/MaskDINO: 10
+tpl/YOLO-v9: 10
+tpl/YOLOX: 10
 tpl/rf-detr: 10
-' "${@}"
+#tpl/segment-anything-2: 10
+#tpl/Open-GroundingDino: 10
+' \
+    --exclude_submodule tpl/segment-anything-2 tpl/Open-GroundingDino \
+    "${@}"
